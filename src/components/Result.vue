c3a8da69f937307fc0ca17b01ab2b8e181d55a09
DerKyela <derkyela@proton.me>
2023-07-02 10:43:37 +0200

    Add copy to clipboard button

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -2,11 +2,19 @@
   <div v-if="canShow">
     <Component
       :is="settings.resultType"
+      ref="result"
       :template="settings.template"
       :list-type="settings.listType"
       :activities="activities"
       :total="total"
     />
+    <button
+      type="button"
+      class="drac-btn drac-border-green drac-btn-outline drac-text-green mt-8"
+      @click="copyResult()"
+    >
+      Copy result to clipboard
+    </button>
   </div>
   <p v-else>
     Please add an activity first.
@@ -14,9 +22,13 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
 import useActivitiesStore from '@/stores/activities';
 import useSettingsStore from '@/stores/settings';
+import type Table from "@/components/Result/Table.vue";
+import type List from "@/components/Result/List.vue";
+
+const result = ref<typeof Table | typeof List | null>(null);
 
 const activitiesStore = useActivitiesStore();
 
@@ -29,4 +41,21 @@ const settings = computed(() => settingsStore.settings);
 const total = computed(() => activitiesStore.total);
 
 const canShow = computed(() => total.value.expectedTime > 0);
+
+function copyResult() {
+  if (result.value) {
+    const clipboardItem = new ClipboardItem({
+      'text/plain': new Blob(
+        [result.value.html.innerText],
+        {type: 'text/plain'},
+      ),
+      'text/html': new Blob(
+        [result.value.html.outerHTML],
+        {type: 'text/html'},
+      ),
+    });
+
+    navigator.clipboard.write([clipboardItem]);
+  }
+}
 </script>
